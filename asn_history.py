3165456556bb302680329fe50b4c05a133ff577f
Raphaël Vinot <raphael@vinot.info>
2013-02-11 16:26:04 +0100

    Better logging and support of import of old files.

--- asn_history.py
+++ asn_history.py
@@ -1,63 +1,120 @@
 #!/usr/bin/python
 
-import re
+import argparse
 import dateutil.parser
-import datetime
-import urllib
+import glob
 import os
+from pubsublogger import publisher
+import re
 import redis
 import time
-from pubsublogger import publisher
+import urllib
 
 import constraint as c
 
 sleep_timer = 36000
 
+def __prepare(directory):
+    temp_dir = os.path.join(directory, 'temp')
+    old_dir = os.path.join(directory, 'old')
+    if not os.path.exists(temp_dir):
+        os.makedirs(temp_dir)
+    if not os.path.exists(old_dir):
+        os.makedirs(old_dir)
+
+def fetch(url, directory):
+    temp_dir = os.path.join(directory, 'temp')
+    old_dir = os.path.join(directory, 'old')
+
+    filename = os.path.join(temp_dir, 'autnums.html')
+    urllib.urlretrieve('http://www.cidr-report.org/as2.0/autnums.html', filename)
+    f = open(filename).read()
+    update_raw = re.sub('[\n()]', '',
+            re.findall('File last modified at (.*)</I>', f , re.S)[0])
+    update = dateutil.parser.parse(update_raw).isoformat()
+
+    newfile = os.path.join(directory, update)
+    oldfile = os.path.join(old_dir, update)
+    if os.path.exists(newfile) or os.path.exists(oldfile):
+        os.remove(filename)
+        return False
+    else:
+        os.rename(filename, newfile)
+        publisher.info('File updated at ' + update)
+        return True
+
+def parse(directory):
+    old_dir = os.path.join(directory, 'old')
+    to_import = glob.glob(os.path.join(directory, '*'))
+    to_import.sort()
+    for f_name in to_import:
+        if os.path.isdir(f_name):
+            continue
+        f = open(f_name).read()
+        data = re.findall('as=AS(.*)&.*</a> (.*)\n', f)
+        update_raw = re.sub('[\n()]', '',
+                re.findall('File last modified at (.*)</I>', f , re.S)[0])
+        update = dateutil.parser.parse(update_raw).isoformat()
+        yield update, data
+        os.rename(f_name, os.path.join(old_dir, update))
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Import ASN descriptions.')
+
+    parser.add_argument('-d', '--directory', type=str, default='autnums',
+            help='Directory where the files are saved.')
+    parser.add_argument('-n', '--not_new', action='store_true',
+            help='Do not download new files from the website.')
+
+    args = parser.parse_args()
+    __prepare(args.directory)
+
     publisher.port = c.redis_port
     publisher.channel = 'ASN_History'
     time.sleep(5)
     publisher.info('Importer started.')
     while True:
-        today = datetime.date.today().strftime("%Y%m%d")
-        autnums_dir = 'autnums'
-        current_file = os.path.join(autnums_dir, today)
+        for timestamp, data in parse(args.directory):
+            r = redis.Redis(host = c.redis_host, port=c.redis_port, db=c.redis_db)
 
-        if os.path.exists(current_file):
-            time.sleep(sleep_timer)
-            continue
-        publisher.info('New day.')
-        urllib.urlretrieve('http://www.cidr-report.org/as2.0/autnums.html', current_file)
+            last_update = r.get('last_update')
+            if last_update > timestamp:
+                msg = 'Trying to import an old file (old). Latest: {new}'.\
+                        format(old=timestamp, new=last_update)
+                publisher.error(msg)
+                continue
+            else:
+                msg = '===== Importing new file: {new} ====='.format(new=timestamp)
+                publisher.info(msg)
+                p = r.pipeline(transaction=False)
+                p.set('last_update', timestamp)
+                p.sadd('all_timestamps', timestamp)
+                new_asns = 0
+                updated_descrs = 0
+                for asn, descr in data:
+                    all_descrs = r.hgetall(asn)
+                    if len(all_descrs) == 0:
+                        p.hset(asn, timestamp, descr)
+                        publisher.debug('New asn: {asn}'.format(asn = asn))
+                        new_asns += 1
+                    else:
+                        dates = all_descrs.keys()
+                        dates.sort()
+                        last_descr = all_descrs[dates[-1]]
+                        if descr != last_descr:
+                            p.hset(asn, timestamp, descr)
+                            msg = 'New description for {asn}. Was {old}, is {new}'.\
+                                    format(asn = asn, old = last_descr, new = descr)
+                            publisher.info(msg)
+                            updated_descrs += 1
+                p.execute()
+                msg = '===== Import finished: {new}, new ASNs:{nb}, Updated:{up} ====='.\
+                        format(new=timestamp, nb=new_asns, up = updated_descrs)
+                publisher.info(msg)
+        if args.not_new:
+            break
+        else:
+            if not fetch('http://www.cidr-report.org/as2.0/autnums.html',
+                    args.directory):
+                time.sleep(sleep_timer)
 
-        f = open(current_file).read()
-        data = re.findall('as=AS(.*)&.*</a> (.*)\n', f)
-        update_raw = re.sub('[\n()]', '',
-                re.findall('File last modified at (.*)</I>', f , re.S)[0])
-
-        update = dateutil.parser.parse(update_raw)
-
-
-        r = redis.Redis(host = c.redis_host, port=c.redis_port, db=c.redis_db)
-
-        last_update = r.get('last_update')
-        if last_update != update.isoformat():
-            msg = 'File has been updated. Was {old} is {new}.'.format(
-                    old=last_update, new = update.isoformat())
-            publisher.info(msg)
-            p = r.pipeline(transaction=False)
-            p.set('last_update', update.isoformat())
-            for asn, descr in data:
-                last_descr = None
-                all_descrs = r.hgetall(asn)
-                if len(all_descrs) != 0:
-                    dates = all_descrs.keys()
-                    dates.sort()
-                    last_descr = all_descrs[dates[-1]]
-                if last_descr is None or descr != last_descr:
-                    p.hset(asn, update.isoformat(), descr)
-                    msg = 'New description for {asn}. Was {old}, is {new}'.\
-                            format(asn = asn, old = last_descr, new = descr)
-                    publisher.info(msg)
-            p.execute()
-            publisher.info('Import finished.')
-        time.sleep(sleep_timer)
